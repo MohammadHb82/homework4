d12e63b1a50ea4f85306f2c038b3dac174dc93fb
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-07-21 07:56:42 -0700

    Create homework4.py
    i add some notes in this task reed them and tell me if i maked some mistakes in this task...

---
+++ homework4.py
@@ -0,0 +1,36 @@
+#counter=1
+#names=["Alica","David","Carolina"]
+#for i in names:
+#    if i=="Alica":
+        #print(counter)
+        #print(i)
+        
+#        names=names[1:]
+#        print(names)
+#    counter=counter+1
+#task1
+        
+        
+        
+        
+#counter=1
+#names=["Alica","David","Carolina"]
+#names.append("Cavin")
+#i will used method append to add a new name to list before i 
+#enter a codition
+#print(names)
+#for i in names:
+#    if i=="Alica":
+        #print(counter)
+        #print(i)
+        
+#        names=names[1:]
+#        print(names)
+#    counter=counter+1
+#names.append("Cavin") 
+#print(names)
+# i will add method append after entering a condition
+        
+#task2 2
+    
+    
